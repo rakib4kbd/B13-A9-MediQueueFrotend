7c2b0a80a6197abcf5e45fe8f258d7ab9b728357
Rakib Khan <rakib4kbd@gmail.com>
2026-05-20 01:03:15 +0600

    booking page

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,48 +1,78 @@
+import CancelBookingButton from "@/components/Booking/CancelBookingButton";
 import { auth } from "@/lib/auth";
-import { Dot } from "lucide-react";
 import { headers } from "next/headers";
 
 const BookedSessionPage = async () => {
   const { user } = await auth.api.getSession({ headers: await headers() });
   const bookedSessionsPromise = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/booking/${user.id}`,
+    { method: "GET", cache: "no-store" },
   );
   const bookedSessions = await bookedSessionsPromise.json();
 
-  console.log(bookedSessions);
   return (
     <div className="container mx-auto my-10">
       <h1 className="text-2xl font-bold mb-4">Booked Sessions</h1>
 
       <div className="overflow-x-auto rounded-box border border-base-content/20 bg-base-200">
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bookedSessions.map((session, index) => (
-              <tr key={session._id}>
-                <th>{index + 1}</th>
-                <td className="font-semibold">{session.tutorName}</td>
-                <td className="uppercase">
-                  <p className="badge badge-info rounded-sm">
-                    {session.status}
-                  </p>
-                </td>
-                <td>
-                  <button className="btn btn-sm btn-error btn-outline">
-                    Cancel Session
-                  </button>
-                </td>
+        {bookedSessions.length === 0 ? (
+          <p className=" flex items-center justify-center p-4 text-center min-h-50">
+            No booked sessions found.
+          </p>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Tutor Name</th>
+                <th>Student Name</th>
+                <th>Status</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {bookedSessions.map((session, index) => (
+                <tr key={session._id}>
+                  <th>{index + 1}</th>
+                  <td className="font-semibold">
+                    <p>{session.tutorName}</p>
+                    <p className="text-xs">
+                      {
+                        new Date(session.bookingDate)
+                          .toISOString()
+                          .split("T")[0]
+                      }
+                    </p>
+                  </td>
+                  <td className="font-semibold">
+                    <p>{session.studentName}</p>
+                    <p className="text-xs">{session.studentEmail}</p>
+                  </td>
+                  <td className="uppercase">
+                    {session.status === "pending" && (
+                      <p className="badge badge-info rounded-sm">
+                        {session.status}
+                      </p>
+                    )}
+                    {session.status === "confirmed" && (
+                      <p className="badge badge-success rounded-sm">
+                        {session.status}
+                      </p>
+                    )}
+                    {session.status === "cancelled" && (
+                      <p className="badge badge-error rounded-sm">
+                        {session.status}
+                      </p>
+                    )}
+                  </td>
+                  <td>
+                    <CancelBookingButton session={session} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
--- app/tutor/[id]/page.js
+++ app/tutor/[id]/page.js
@@ -18,7 +18,6 @@ const TutorDetailPage = async ({ params }) => {
   const { id } = await params;
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tutor/${id}`);
   const tutor = await res.json();
-  console.log(tutor);
   return (
     <div className="bg-base-200">
       <div className="container mx-auto py-5">
